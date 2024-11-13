01ff1bbf3a0d7db7474cfc065374d21d01fdd1ba
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-11-12 22:03:30 -0300

    Primeiro Commit

---
+++ DAO/Conexao.php
@@ -0,0 +1,30 @@
+<?php
+    namespace PHP\Modelo\DAO;
+
+    use Exception;
+
+    class Conexao{
+
+        function conectar(){ // metodo para se ligar com o banco de dados
+
+            try{
+
+                $conn = mysqli_connect("localhost","root","","AmazonPHP");
+
+                if($conn){
+                    echo "<br>Conectado com sucesso!";
+                    return $conn;
+                }
+                echo "<br> Algo deu errado";
+
+            }catch(Exception $erro){
+
+                return "Algo deu errado".$erro;
+
+            }
+
+
+        }// metodo     
+
+    }// criei a classe conexão
+?>
